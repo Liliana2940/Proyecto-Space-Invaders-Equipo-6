36065599c0c0c9d043dabb91d512f3f9413944ba
EvanDJ57 <90639500+EvanDJ57@users.noreply.github.com>
2022-09-12 00:00:07 -0500

    Add files via upload

---
+++ Invader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invader : MonoBehaviour
+{
+    public Sprite[] animationSprites;
+    public float animationTime = .25f;
+    private SpriteRenderer _spriteRenderer;
+    private int _animationFrame;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), this.animationTime, this.animationTime);
+    }
+
+    private void AnimateSprite()
+    {
+        _animationFrame++;
+        if (_animationFrame >= animationSprites.Length)
+        {
+            _animationFrame = 0;
+        }
+        _spriteRenderer.sprite = this.animationSprites[_animationFrame];
+    }
+}
